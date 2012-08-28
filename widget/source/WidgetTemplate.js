ffb518ddcc8374e0e2a53e3d9c68a6767114a2a0
Josh Palmer <jpalmerwatkins@me.com>
2012-08-28 04:14:08 +0100

    Initial Commit

---
+++ widget/source/WidgetTemplate.js
@@ -0,0 +1,7 @@
+enyo.kind({
+	name: "Widget",
+	style: "width:240px; height:60px; top:90px; left:40px; border-radius:15px; background-color:grey; color:white;",
+	components:[
+		{content: "I ARE BIG WIDGET, THORGLE BORGLE!!!", style: "padding:10px;"}
+	]
+});
---
+++ widget/source/package.js
@@ -0,0 +1,3 @@
+enyo.depends(
+	"WidgetTemplate.js"
+);
